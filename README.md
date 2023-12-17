b86f14e5a2971c1c4b0df570e6d89664546f3cc3
gnatyk-dmytro <gnatykwork@gmail.com>
2023-12-17 21:26:45 +0100

    bozon-io CryptoBot v1.0

---
+++ README.md
@@ -0,0 +1,39 @@
+# bozon:io - Cryptocurrency TelegramBot
+
+## Welcome to bozon:io!
+
+bozon:io is a project dedicated to creating various projects related to cryptocurrencies. The first project is a Telegram Bot implemented in C# that provides data on different cryptocurrencies.
+
+## Project Overview
+
+### bozonBot - Cryptocurrency TelegramBot
+
+- **Description:** bozonBot is a Telegram Bot developed in C# that allows users to easily access information about various cryptocurrencies.
+
+- **Features:**
+  - Real-time data on cryptocurrency prices.
+  - Information about market trends and statistics.
+  - User-friendly commands for seamless interaction.
+
+## Getting Started
+
+To use bozonBot, follow these steps:
+
+1. **Clone the Repository:**
+   ```bash
+   git clone https://github.com/[YourUsername]/bozonBot.git
+   cd bozonBot
+   ```
+2. **Set Up Your Environment:**
+   - Make sure you have the necessary dependencies installed.
+   - Update your configuration with the required API keys and credentials.
+3. **Build and Run:**
+   ```bash
+   dotnet build 
+   ```
+   ```bash
+   dotnet run
+   ```
+## License
+
+[MIT]](https://mit-license.org/)
---
+++ Src/Program.cs
@@ -0,0 +1,123 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+
+namespace Src
+{
+    internal class Program
+    {
+        private static ITelegramBotClient? _botClient;
+        private static ReceiverOptions? _receiverOptions;
+
+        public static async Task Main(string[] args)
+        {
+            _botClient = new TelegramBotClient("YOUR_TOKEN");
+            _receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = new[]
+                {
+                    UpdateType.Message,
+                    UpdateType.CallbackQuery,
+                },
+                ThrowPendingUpdates = true
+            };
+
+            using var ctx = new CancellationTokenSource();
+
+            _botClient.StartReceiving(UpdateHandler, ErrorHandler, _receiverOptions, ctx.Token);
+            var botSettings = await _botClient.GetMeAsync(ctx.Token);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"#{botSettings.Username} start working>>>\n");
+            Console.ResetColor();
+
+            await Task.Delay(-1, ctx.Token);
+        }
+
+        private static async Task UpdateHandler(ITelegramBotClient botClient, Update update, CancellationToken ctx)
+        {
+            try
+            {
+                switch (update.Type)
+                {
+                    case UpdateType.Message:
+                        var message = update.Message;
+                        var chat = message!.Chat;
+                        var from = message!.From;
+
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine($"#{from!.Username} type text message: {message.Text}\n");
+                        Console.ResetColor();
+
+                        if (message.Text == "/start")
+                        {
+                            await botClient.SendTextMessageAsync
+                                (
+                                chat.Id,
+                                $"Welcome to the  #bozonBot:  @{from!.Username}\nType - /info for more Information",
+                                cancellationToken: ctx
+                                );
+                        }
+                        else if (message.Text == "/info")
+                        {
+                            await botClient.SendTextMessageAsync
+                                (
+                                chat!.Id,
+                                $"We are the bozon:io team working on cryptocurrencies,\nAnd our mission is to optimize data acquisition from the CRYPTOS\n\nFunctions:\n\n/follow - Follow on Updates\n/currency - Show all available Currency",
+                                cancellationToken: ctx
+                                );
+                        }
+                        else if (message.Text == "/currency")
+                        {
+                            await botClient.SendTextMessageAsync(
+                                chat!.Id,
+                                $"All Currency:\n\nBitcoin - /bitcoin\nEthereum - /eth\nToncoin - /ton\nBNB - /bnb",
+                                cancellationToken: ctx
+                            );
+                        }
+                        else if (message.Text == "/follow")
+                        {
+                            await botClient.SendTextMessageAsync(
+                                chat!.Id,
+                                "\nFollow function is not available in this moment",
+                            cancellationToken: ctx
+                            );
+                        }
+                        else if (message.Text == "/bitcoin")
+                        {
+                            await botClient.SendTextMessageAsync(
+                                chat!.Id,
+                                "Bitcoin price:",
+                            cancellationToken: ctx
+                                );
+                        }
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine(ex.ToString());
+                Console.ResetColor();
+            }
+        }
+
+        private static Task ErrorHandler(ITelegramBotClient botClient, Exception ex, CancellationToken ctx)
+        {
+            var error = ex switch
+            {
+                ApiRequestException apiRequestException
+                => "Telegram Api Request Exception" + apiRequestException,
+                _ => ex.ToString()
+            };
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(error);
+            Console.ResetColor();
+
+            return Task.CompletedTask;
+        }
+    }
+}
